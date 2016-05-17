73ab17309ea3398f43042f12aaf5ad43c366de7e
fivdi <bdscooke@gmail.com>
2016-05-17 23:20:04 +0200

    fixed typo

--- example/mcp3008-tmp36.js
+++ example/mcp3008-tmp36.js
@@ -7,7 +7,7 @@ var spi = require('bindings')('spi');
 
 // The MCP3008 is on bus 0 and it's device 0
 var mcp3008 = spi.open(0, 0, function (err) {
-  // An SPI message is an array of one or read+write transfers
+  // An SPI message is an array of one or more read+write transfers
   var message = [{
     sendBuffer: new Buffer([0x01, 0xd0, 0x00]), // Sent to read channel 5
     receiveBuffer: new Buffer(3),               // Raw data read from channel 5
