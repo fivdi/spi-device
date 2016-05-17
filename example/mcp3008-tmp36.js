cf24641660be82b44b28485d3b3fa1117921a902
fivdi <bdscooke@gmail.com>
2016-05-17 21:34:24 +0200

    comment example

--- example/mcp3008-tmp36.js
+++ example/mcp3008-tmp36.js
@@ -2,14 +2,19 @@
 
 var spi = require('bindings')('spi');
 
-var message = [{
-  sendBuffer: new Buffer([0x01, 0xd0, 0x00]),
-  receiveBuffer: new Buffer(3),
-  byteLength: 3,
-  speed: 20000
-}];
+// Determine the temperature using a TMP36 analog temperature sensor wired to
+// channel 5 on an MCP3008 SPI A/D converter
 
+// The MCP3008 is on bus 0 and it's device 0
 var mcp3008 = spi.open(0, 0, function (err) {
+  // An SPI message is an array of one or read+write transfers
+  var message = [{
+    sendBuffer: new Buffer([0x01, 0xd0, 0x00]), // Sent to read channel 5
+    receiveBuffer: new Buffer(3),               // Raw data read from channel 5
+    byteLength: 3,
+    speed: 20000 // Use a low bus speed to get a good reading from the TMP36
+  }];
+
   if (err) {
     throw err;
   }
@@ -23,6 +28,7 @@ var mcp3008 = spi.open(0, 0, function (err) {
       throw err;
     }
 
+    // Convert raw value from sensor to celcius and log to console
     rawValue = (message[0].receiveBuffer[1] << 8) +
       message[0].receiveBuffer[2];
     voltage = rawValue * 3.3 / 1024;
