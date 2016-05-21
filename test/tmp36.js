e7df1561dad92d9edc8c1109603f7802b56a5be6
fivdi <bdscooke@gmail.com>
2016-05-21 13:42:55 +0200

    open asynchronously in asynchronous test

--- test/tmp36.js
+++ test/tmp36.js
@@ -1,37 +1,45 @@
 'use strict';
 
 var spi = require('bindings')('spi');
-var mcp3008 = spi.openSync(0, 0);
+var assert = require('assert');
+var mcp3008;
 var rawValue;
 var celcius;
 var count = 0;
 var total = 0;
 
-var message = [{
-  sendBuffer: new Buffer([0x01, 0xd0, 0x00]),
-  receiveBuffer: new Buffer([0, 0, 0]),
-  byteLength: 3,
-  speed: 20000
-}];
+mcp3008 = spi.open(0, 0, function (err) {
+  var message = [{
+    sendBuffer: new Buffer([0x01, 0xd0, 0x00]),
+    receiveBuffer: new Buffer([0, 0, 0]),
+    byteLength: 3,
+    speed: 20000
+  }];
 
-(function next() {
-  mcp3008.transfer(message, function (err, message) {
-    var voltage;
+  assert(!err, 'can\'t open device');
 
-    count += 1;
+  (function next() {
+    mcp3008.transfer(message, function (err, message) {
+      var voltage;
 
-    rawValue = (message[0].receiveBuffer[1] << 8) +
-      message[0].receiveBuffer[2];
-    voltage = rawValue * 3.3 / 1024;
-    celcius = (voltage - 0.5) * 100;
+      assert(!err, 'can\'t transfer message');
 
-    next();
-  });
-}());
+      count += 1;
 
-setInterval(function () {
-  total += count;
-  console.log(count + ' ' + total + ' ' + rawValue + ' '  + celcius);
-  count = 0;
-}, 1000);
+      rawValue = (message[0].receiveBuffer[1] << 8) +
+        message[0].receiveBuffer[2];
+      voltage = rawValue * 3.3 / 1024;
+      celcius = (voltage - 0.5) * 100;
+
+      next();
+    });
+  }());
+
+  setInterval(function () {
+    total += count;
+    console.log(count + ' ' + total + ' ' + rawValue + ' '  + celcius);
+    count = 0;
+  }, 1000);
+});
+
 
